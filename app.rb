63ba3255f7b32f29962c8d372cd1e984b603ac39
Andrew Kloecker <lt.kvothe@gmail.com>
2020-06-05 11:46:57 -0400

    Done.

--- app.rb
+++ app.rb
@@ -1,7 +1,6 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
-
   # This is a sample static route.
   get '/hello' do
     "Hello World!"
@@ -14,5 +13,18 @@ class App < Sinatra::Base
   end
 
   # Code your final two routes here:
+  get '/medicines/:id' do
+    @medicine = all_the_medicines.select do |medicine|
+      medicine.id == params[:id]
+    end.first
+    erb :'/medicines/show.html'
+  end
 
+  get '/goodbye/:name' do
+    "Goodbye, #{params[:name]}."
+  end
+
+  get '/multiply/:num1/:num2' do
+    "#{params[:num1].to_i * params[:num2].to_i}"
+  end
 end
